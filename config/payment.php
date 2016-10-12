ca06172307b7beb90435a4370bc73f4550279a13
duonglt <duonglt@fsoft.com.vn>
2016-10-13 00:28:09 +0700

    duonglt

--- config/payment.php
+++ config/payment.php
@@ -2,7 +2,7 @@
 return [
   'pay_list'=>[
       "pay_packages"=>[
-          ['code'=>"152.1", 'amount'=>100000, "product_name"=>"Nạp 100.000 đ vào tài khoản"],
+          ['code'=>"152.1", 'amount'=>11000, "product_name"=>"Nạp 11.000 đ vào tài khoản"],
           ['code'=>"152.2", 'amount'=>200000, "product_name"=>"Nạp 200.000 đ vào tài khoản"],
           ['code'=>"152.3", 'amount'=>300000, "product_name"=>"Nạp 300.000 đ vào tài khoản"],
           ['code'=>"152.4", 'amount'=>400000, "product_name"=>"Nạp 400.000 đ vào tài khoản"],
